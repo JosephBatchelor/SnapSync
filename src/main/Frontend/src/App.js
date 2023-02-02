cc86f6d91602aa38b3c1c3ff4b4bbcccedbba377
Joseph Batchelor <65980604+JosephBatchelor@users.noreply.github.com>
2023-02-02 10:19:20 +0000

    +Frontend

---
+++ src/main/Frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import React, {useState, useEffect, useCallback} from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> "UI design is currently being reworked, I will update this soon!" </h1>
+    </div>
+  );
+}
+
+export default App;
