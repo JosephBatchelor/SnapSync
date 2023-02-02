0582f8387aee62051bfa361e29bd110505e86467
Joseph Batchelor <65980604+JosephBatchelor@users.noreply.github.com>
2023-02-02 10:15:17 +0000

    Add files via upload

---
+++ src/test/java/com/JoeBatchelor/DigitalLedger/DigtialLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JoeBatchelor.DigitalLedger;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DigtialLedgerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
